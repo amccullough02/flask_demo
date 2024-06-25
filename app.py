61f06e839fbb3f61aff441b45f179dea78868b5c
Alex McCullough <alexmccullough@outlook.com>
2024-06-25 15:39:34 +0100

    add: todo sample data

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
+todo_list = [{"todo": "Get Revenge on Jason", "done": False}]
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", todos=todo_list)
 
 # Add CRUD methods and a check method to mark item as done.
 
