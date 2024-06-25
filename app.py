ca8f34ba56e1d2dc2db21df96b0a02c56b165219
Alex McCullough <alexmccullough@outlook.com>
2024-06-25 15:43:36 +0100

    add: add function

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
-todo_list = [{"todo": "Get Revenge on Jason", "done": False}]
+todo_list = [{"task": "Get Revenge on Jason", "done": False}]
 
 @app.route('/')
 def index():
     return render_template("index.html", todos=todo_list)
 
 # Add CRUD methods and a check method to mark item as done.
+
+@app.route('/add', methods=["POST"])
+def add():
+    todo = request.form['todo']
+    todo_list.append({"task": todo, "done": False})
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
