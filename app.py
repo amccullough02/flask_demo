b808949ca7bbba4260850706b7070045132192f2
Alex McCullough <alexmccullough@outlook.com>
2024-10-13 17:55:48 +0100

    chore: pep8

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ app = Flask(__name__, template_folder="templates")
 
 todo_list = [{"task": "Get Revenge on Jason", "done": False}]
 
+
 @app.route("/")
 def index():
     return render_template("index.html", todos=todo_list)
+
 
 @app.route("/add", methods=["POST"])
 def add():
     todo = request.form['todo']
     todo_list.append({"task": todo, "done": False})
     return redirect(url_for("index"))
+
 
 @app.route("/edit/<int:index>", methods=["GET", "POST"])
 def edit(index):
@@ -23,15 +26,18 @@ def edit(index):
     else:
         return render_template("edit.html", todo=todo, index=index)
 
-@app.route("/check/<int:index>")    
+
+@app.route("/check/<int:index>")
 def check(index):
     todo_list[index]['done'] = not todo_list[index]['done']
     return redirect(url_for("index"))
+
 
 @app.route("/delete/<int:index>")
 def delete(index):
     del todo_list[index]
     return redirect(url_for("index"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
